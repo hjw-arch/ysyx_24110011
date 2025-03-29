0a8d4e8d83e53be826c666b9cb828428b01eb0a2
tracer-ysyx <tracer@ysyx.org>
2025-03-29 15:55:19 +0800

    > compile NEMU ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:55:19 up 11:57,  1 user,  load average: 0.32, 0.24, 0.22

--- nemu/include/memory/paddr.h
+++ nemu/include/memory/paddr.h
@@ -27,9 +27,27 @@ uint8_t* guest_to_host(paddr_t paddr);
 /* convert the host virtual address in NEMU to guest physical address in the guest program */
 paddr_t host_to_guest(uint8_t *haddr);
 
+#ifndef CONFIG_SOC
 static inline bool in_pmem(paddr_t addr) {
   return addr - CONFIG_MBASE < CONFIG_MSIZE;
 }
+
+#else
+
+#define ROM_BASE	0x20000000
+#define ROM_SIZE	0x1000
+#define ROM_END		ROM_BASE + ROM_SIZE - 1
+#define ROM_MASK	0xFFFFF000
+
+#define SRAM_BASE	0x0f000000
+#define SRAM_SIZE	0x2000
+#define SRAM_END	SRAM_BASE + SRAM_SIZE - 1
+#define SRAM_MASK	0xFFFFE000
+static inline bool in_pmem(paddr_t addr) {
+	return (addr - ROM_BASE < ROM_SIZE) || (addr - SRAM_BASE < SRAM_SIZE);
+}
+
+#endif
 
 word_t paddr_read(paddr_t addr, int len);
 void paddr_write(paddr_t addr, int len, word_t data);
