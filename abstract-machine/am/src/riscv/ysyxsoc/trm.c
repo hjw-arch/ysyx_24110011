357a873ca1200b3b2ef0e167d4feefb4d5572dfc
tracer-ysyx <tracer@ysyx.org>
2025-03-30 11:47:57 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:47:57 up 21:55,  1 user,  load average: 0.27, 0.25, 0.25

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -3,30 +3,47 @@
 #include <klib-macros.h>
 #include <riscv.h>
 
-extern char _heap_start;
+extern char _heap_start, _heap_end;
 int main(const char *args);
 
-extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
-#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
+Area heap = RANGE(&_heap_start, &_heap_end);
 
-Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
+
 static const char mainargs[] = MAINARGS;
 
-#define SERIAL_PORT	0x10000000
+#define SERIAL_PORT 0x10000000
 void putch(char ch) {
-  outb(SERIAL_PORT, ch);
+	outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
-  asm volatile("ebreak");
-  while (1);
+	asm volatile("ebreak");
+	while (1);
+}
+
+void bootloader() {
+	extern char _data_vma_start, _data_lma_start, _bss_start;
+	extern char _data_size, _bss_size;
+	uint8_t* data_vma_start = &_data_vma_start;
+	uint8_t* data_lma_start = &_data_lma_start;
+	uint8_t* bss_start = &_bss_start;
+	uint32_t data_size = &_data_size;
+	uint32_t bss_size = &_bss_size;
+
+	for (int i = 0; i < data_size; i++) {
+		data_vma_start[i] = data_lma_start[i];
+	}
+
+	for (int i = 0; i < bss_size; i++) {
+		bss_start[i] = 0;
+	}
 }
 
 void _trm_init() {
-  int ret = main(mainargs);
-  halt(ret);
+
+	int ret = main(mainargs);
+	halt(ret);
 }
