e854baee73837f28eb25b304d186c485130a441b
tracer-ysyx <tracer@ysyx.org>
2025-03-31 12:25:02 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:25:02 up 1 day, 14:07,  1 user,  load average: 0.39, 0.33, 0.35

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -14,9 +14,32 @@ Area heap = RANGE(&_heap_start, &_heap_end);
 
 static const char mainargs[] = MAINARGS;
 
-#define SERIAL_PORT 0x10000000
+#define UART_BASE 	0x10000000
+#define UART_TX		(UART_BASE + 0x00)
+#define UART_RX		(UART_BASE + 0x00)
+#define UART_IER	(UART_BASE + 0x01)
+#define UART_IIR	(UART_BASE + 0x02)
+#define UART_FCR	(UART_BASE + 0x02)
+#define UART_LCR	(UART_BASE + 0x03)
+#define UART_MC		(UART_BASE + 0x04)
+#define	UART_LSR	(UART_BASE + 0x05)
+#define UART_MS		(UART_BASE + 0x06)
+
+#define UART_LSB	(UART_BASE + 0x00)
+#define UART_MSB	(UART_BASE + 0x01)
+
+#define UART_FIFO_EMPTY_MASK	0x20
+
+void uart_init() {
+	*(volatile uint8_t *)UART_LCR = 0x83;	// 0b10000011
+	*(volatile uint8_t *)UART_MSB = 0x00;
+	*(volatile uint8_t *)UART_LSB = 0x01;
+	*(volatile uint8_t *)UART_LCR = 0x03;
+}
+
 void putch(char ch) {
-	outb(SERIAL_PORT, ch);
+	while((*(uint8_t *)UART_LSR & UART_FIFO_EMPTY_MASK) == 0);	// 等待
+	outb(UART_TX, ch);
 }
 
 void halt(int code) {
@@ -25,6 +48,7 @@ void halt(int code) {
 }
 
 void _trm_init() {
+	uart_init();
 	int ret = main(mainargs);
 	halt(ret);
 }
