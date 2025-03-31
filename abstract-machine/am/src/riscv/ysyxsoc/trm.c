ed9e3a13f9d3910fbde88db02b6525a3bd7b6c63
Hjw-Arch <hujinwei_121@163.com>
2025-03-31 12:54:23 +0800

    FLASH START

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -14,9 +14,32 @@ Area heap = RANGE(&_heap_start, &_heap_end);
 
 static const char mainargs[] = MAINARGS;
 
-#define SERIAL_PORT 0x10000000
+#define UART_BASE 	0x10000000
+#define UART_TX		*(volatile uint8_t *)(UART_BASE + 0x00)
+#define UART_RX		*(volatile uint8_t *)(UART_BASE + 0x00)
+#define UART_IER	*(volatile uint8_t *)(UART_BASE + 0x01)
+#define UART_IIR	*(volatile uint8_t *)(UART_BASE + 0x02)
+#define UART_FCR	*(volatile uint8_t *)(UART_BASE + 0x02)
+#define UART_LCR	*(volatile uint8_t *)(UART_BASE + 0x03)
+#define UART_MC		*(volatile uint8_t *)(UART_BASE + 0x04)
+#define	UART_LSR	*(volatile uint8_t *)(UART_BASE + 0x05)
+#define UART_MS		*(volatile uint8_t *)(UART_BASE + 0x06)
+
+#define UART_LSB	*(volatile uint8_t *)(UART_BASE + 0x00)
+#define UART_MSB	*(volatile uint8_t *)(UART_BASE + 0x01)
+
+#define UART_FIFO_EMPTY_MASK	1 << 5
+
+void uart_init() {
+	UART_LCR = 0x83;	// 0b10000011
+	UART_MSB = 0x00;
+	UART_LSB = 0x01;
+	UART_LCR = 0x03;
+}
+
 void putch(char ch) {
-	outb(SERIAL_PORT, ch);
+	while((UART_LSR & UART_FIFO_EMPTY_MASK) == 0);	// 等待
+	UART_TX = ch;
 }
 
 void halt(int code) {
@@ -25,6 +48,7 @@ void halt(int code) {
 }
 
 void _trm_init() {
+	uart_init();
 	int ret = main(mainargs);
 	halt(ret);
 }
