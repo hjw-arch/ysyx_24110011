0521a86786937303345310dec0d2afcdf51b952b
tracer-ysyx <tracer@ysyx.org>
2025-04-02 22:23:33 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:23:33 up 3 days,  4:36,  1 user,  load average: 0.25, 0.25, 0.25

--- npc/csrc/src/monitor.cpp
+++ npc/csrc/src/monitor.cpp
@@ -100,9 +100,18 @@ static int parse_args(int argc, char *argv[]) {
 
 extern uint8_t flash[0x10000000];
 void flash_init() {
-	for (int i = 0; i < img_size; i++) {
-		flash[i] = ((uint8_t *)(guest_to_host(RESET_VECTOR)))[i];
+	FILE *file = fopen("/home/hjw-arch/ysyx-workbench/npc/ROM/bin/UART.bin", "rb");
+	Assert(file, "Open .bin fail.\n");
+	fseek(file, 0, SEEK_END);
+	uint32_t file_size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	int ret = fread(flash, file_size, 1, file);
+	if (ret != 1) {
+		Assert(0, "fread fail.\n");
 	}
+
+	fclose(file);
 }
 
 
--- ysyxSoC/perip/spi/rtl/spi_top_apb.v
+++ ysyxSoC/perip/spi/rtl/spi_top_apb.v
@@ -48,18 +48,120 @@ assign in_prdata  = data[31:0];
 
 `else
 
+
+localparam SPI_CTRL_GO_BUY	= 8;
+
+// 状态
+localparam NORMAL				= 0;
+localparam XIP_SEND_ADDR		= 1;
+localparam XIP_CONF_DIV			= 2;
+localparam XIP_CONF_SS			= 3;
+localparam XIP_CONF_CTRL_START	= 4;
+localparam XIP_WAIT_DATA		= 5;
+localparam XIP_RESET_SS			= 6;
+
+reg [3:0] state;
+
+// 输出
+assign in_pslverr = (in_paddr[31:28] == 4'h3 && in_psel && in_pwrite) ? 1'b1 : spi_pslverr;
+assign in_pready = (in_paddr[31:28] == 4'h3 && in_psel && in_penable && in_pwrite) ? 1'b1 : (is_xip ? xip_pready : spi_pready);
+assign in_prdata = is_xip ? xip_prdata : spi_prdata;
+
+// XIP内部信号
+wire [4:0] xip_paddr;
+wire [31:0] xip_pwdata;
+wire [31:0] xip_prdata;
+reg xip_penable;
+wire xip_pready;
+wire xip_pwrite;
+
+// 给定spi_top输入
+wire [4:0] spi_paddr;
+wire [31:0] spi_pwdata;
+wire [3:0] spi_pstrb;
+wire spi_pwrite;
+wire spi_penable;
+wire spi_psel = in_psel;
+
+// 给定spi_top输出
+wire spi_pready;
+wire spi_pslverr;
+wire [31:0] spi_prdata;
+
+
+// XIP状态判断
+wire is_xip = state != NORMAL;
+
+// 输入信号多路复用
+assign spi_paddr = is_xip ? xip_paddr[4:0] : in_paddr[4:0];
+assign spi_pwdata = is_xip ? xip_pwdata : in_pwdata;
+assign spi_pstrb = is_xip ? 4'b1111 : in_pstrb;
+assign spi_pwrite = is_xip ? xip_pwrite : in_pwrite;
+assign spi_penable = is_xip ? xip_penable : in_penable;
+
+// 状态转移
+wire is_flash_access = in_paddr[31:28] == 4'h3 && in_psel && !in_pwrite;
+always @(posedge clock) begin
+	if (reset) begin
+		state <= NORMAL;
+	end else begin
+		case(state)
+			NORMAL:
+				state <= is_flash_access ? XIP_SEND_ADDR : state;
+			XIP_SEND_ADDR:
+				state <= (xip_penable & spi_pready) ? XIP_CONF_DIV : state;
+			XIP_CONF_DIV:
+				state <= (xip_penable & spi_pready) ? XIP_CONF_SS : state;
+			XIP_CONF_SS:
+				state <= (xip_penable & spi_pready) ? XIP_CONF_CTRL_START : state;
+			XIP_CONF_CTRL_START:
+				state <= (xip_penable & spi_pready) ? XIP_WAIT_DATA : state;
+			XIP_WAIT_DATA:
+				state <= (xip_penable & spi_pready & !spi_prdata[SPI_CTRL_GO_BUY]) ? XIP_RESET_SS : state;
+			XIP_RESET_SS:
+				state <= (xip_penable & spi_pready) ? NORMAL : state;
+			default:
+				state <= NORMAL;
+	endcase
+	end
+end
+
+// penable
+always @(posedge clock) begin
+	xip_penable <= (xip_penable & spi_pready & !reset) ? 1'b0 : 1'b1;
+end
+
+// paddr
+assign xip_paddr = 	{5{(state == XIP_SEND_ADDR)}} & 5'h04 | {5{state == XIP_CONF_DIV}} & 5'h14 | {5{state == XIP_CONF_SS}} & 5'h18 |
+					{5{state == XIP_CONF_CTRL_START}} & 5'h10 | {5{state == XIP_WAIT_DATA}} & 5'h10 | {5{state == XIP_RESET_SS}} & 5'h18;
+
+// pwdata
+assign xip_pwdata = {32{(state == XIP_SEND_ADDR)}} & {8'h03, in_paddr[23:2], 2'b0} | {32{state == XIP_CONF_DIV}} & 32'h01 | {32{state == XIP_CONF_SS}} & 32'h01 |
+					{32{state == XIP_CONF_CTRL_START}} & 32'h540 | {32{state == XIP_RESET_SS}} & 32'h00;			// XIP_WAIT_DATA只读不写
+
+// prdata
+// 大端转小端
+assign xip_prdata = {spi_prdata[7:0], spi_prdata[15:8], spi_prdata[23:16], spi_prdata[31:24]};
+
+// pwrite
+assign xip_pwrite = (state != XIP_WAIT_DATA);		// 只有这个阶段需要读
+
+// pready
+assign xip_pready = (state == XIP_RESET_SS) & spi_pready & in_penable;
+
+
 spi_top u0_spi_top (
   .wb_clk_i(clock),
   .wb_rst_i(reset),
-  .wb_adr_i(in_paddr[4:0]),
-  .wb_dat_i(in_pwdata),
-  .wb_dat_o(in_prdata),
-  .wb_sel_i(in_pstrb),
-  .wb_we_i (in_pwrite),
-  .wb_stb_i(in_psel),
-  .wb_cyc_i(in_penable),
-  .wb_ack_o(in_pready),
-  .wb_err_o(in_pslverr),
+  .wb_adr_i(spi_paddr[4:0]),
+  .wb_dat_i(spi_pwdata),
+  .wb_dat_o(spi_prdata),
+  .wb_sel_i(spi_pstrb),
+  .wb_we_i (spi_pwrite),
+  .wb_stb_i(spi_psel),
+  .wb_cyc_i(spi_penable),
+  .wb_ack_o(spi_pready),
+  .wb_err_o(spi_pslverr),
   .wb_int_o(spi_irq_out),
 
   .ss_pad_o(spi_ss),
@@ -67,6 +169,13 @@ spi_top u0_spi_top (
   .mosi_pad_o(spi_mosi),
   .miso_pad_i(spi_miso)
 );
+
+// always @(posedge clock) begin
+// 	if (is_xip) begin
+// 		if (state == NORMAL) $display("111");
+// 	end
+// end
+
 
 `endif // FAST_FLASH
 
