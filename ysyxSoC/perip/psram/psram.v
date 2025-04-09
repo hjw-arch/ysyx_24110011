570525eb7db0d1989e3a146d1c15526d3430f4fd
tracer-ysyx <tracer@ysyx.org>
2025-04-09 11:08:25 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:08:25 up 2 days, 18:10,  1 user,  load average: 0.25, 0.22, 0.16

--- ysyxSoC/perip/psram/psram.v
+++ ysyxSoC/perip/psram/psram.v
@@ -1,9 +1,133 @@
 module psram(
-  input sck,
-  input ce_n,
-  inout [3:0] dio
+      input sck,
+      input ce_n,
+      inout [3:0] dio
 );
 
-  assign dio = 4'bz;
+// 命令定义
+localparam CMD_Q_READ    =     8'hEB;
+localparam CMD_Q_WRITE    =    8'h38;
+
+localparam S_IDLE     = 1;
+localparam S_CMD    = 2;
+localparam S_ADDR    = 3;
+localparam S_DUMMY     = 4;
+localparam S_READ     = 5;
+localparam S_WRITE    = 6;
+
+// localparam ADDR_BITS  = 24;
+reg [7 : 0] PSRAM[0 : (1 << 22) - 1];
+
+
+logic [2 : 0] state, next_state;
+reg [2 : 0] cnt;
+reg [7 : 0] cmd;
+reg [23 : 0] addr;
+
+reg dio_oe;        // dio输出使能信号
+reg [3:0] dio_out;
+wire [3:0] dio_in;
+
+assign dio = dio_oe ? dio_out : 4'bz;
+assign dio_in = dio;
+
+// 状态转移
+always_ff @(posedge sck or posedge ce_n) begin
+    state <= ce_n ? S_IDLE : next_state;
+end
+
+always_comb begin
+    case(state) 
+        S_IDLE:
+            next_state = ~ce_n ? S_CMD : state;
+        S_CMD:
+            next_state = cnt == 3'b111 ? S_ADDR : state;
+        S_ADDR:
+            next_state = cnt == 3'd5 && cmd == CMD_Q_READ ? S_DUMMY : cnt == 3'd5 && cmd == CMD_Q_WRITE ? S_WRITE : state;
+        S_DUMMY:
+            next_state = cnt == 3'd5 ? S_READ : state;
+        S_READ:
+            next_state = state;
+        S_WRITE:
+            next_state = state;
+        default: next_state = state;
+    endcase
+end
+
+// 时钟
+always_ff @(posedge sck or posedge ce_n) begin
+    if (ce_n) begin
+        cnt <= 0;
+    end else begin
+        case(state)
+            S_IDLE: cnt <= 3'b0;
+            S_CMD:    cnt <= cnt == 3'b111 ? 3'b0 : cnt + 1;
+            S_ADDR:    cnt <= cnt == 3'b101 ? 3'b0 : cnt + 1;
+            S_DUMMY: cnt <= cnt == 3'b101 ? 3'b0 : cnt + 1;
+            default: cnt <= 0;
+        endcase
+    end
+end
+
+// cmd
+always_ff @(posedge sck) begin
+    cmd <= state == S_CMD ? {cmd[6 : 0], dio_in[0]} : cmd;
+end
+
+// addr
+always_ff @(posedge sck) begin
+    if (state == S_ADDR) begin
+        addr <= {addr[19 : 0], dio_in};
+    end else if (state == S_READ && ~data_toggle || state == S_WRITE && ~data_toggle) begin
+        addr <= addr + 1;
+    end else begin
+        addr <= addr;
+    end
+end
+
+// read & write
+assign dio_oe = state == S_READ;
+reg [3 : 0] write_buf;    // 用于暂存写数据
+reg [7 : 0] read_buf;   // 暂存读数据
+reg data_toggle;
+
+always_ff @(posedge sck) begin
+    data_toggle <= cnt == 3'b101 ? 1'b1 : (state == S_READ || state == S_WRITE) ? ~data_toggle : data_toggle;
+end
+
+// read
+// 功能仿真
+always_ff @(posedge sck) begin
+    if (state == S_DUMMY && cnt == 3'b101 || state == S_READ && ~data_toggle) begin
+        read_buf <= PSRAM[addr[21:0]];
+    end else begin
+        read_buf <= read_buf;
+    end
+end
+
+
+always_ff @(posedge sck) begin
+    if (state == S_READ) begin
+        if (data_toggle) begin
+            dio_out <= read_buf[7 : 4];
+        end else begin
+            dio_out <= read_buf[3 : 0];
+        end
+    end
+end
+
+
+// write
+// 功能仿真
+always_ff @(posedge sck) begin
+    if (state == S_WRITE) begin
+        if (data_toggle) begin
+            write_buf <= dio_in;
+        end else begin
+            PSRAM[addr[21:0]] <= {write_buf, dio_in};
+        end
+    end
+end
+
 
 endmodule
