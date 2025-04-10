bea9efa086ceb3618d15954c2562610c06088407
tracer-ysyx <tracer@ysyx.org>
2025-04-11 00:18:48 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:18:48 up 14:08,  1 user,  load average: 0.12, 0.18, 0.18

--- ysyxSoC/perip/psram/psram.v
+++ ysyxSoC/perip/psram/psram.v
@@ -56,10 +56,6 @@ always_comb begin
     endcase
 end
 
-// always_ff @(posedge sck) begin
-// 	$display("In PSRAM, dio_in = %x, cmd = %02x, cnt = %d, ce = %d, mode = %d", dio_in[0], {cmd[6 : 0], dio_in[0]}, cnt, ce_n, qpi_mode);
-// end
-
 // 模式切换
 always_ff @(posedge sck) begin
 	qpi_mode <= !qpi_mode && cnt == 3'b111 && {cmd[6:0], dio_in[0]} == CMD_ENTER_QPI ? 1'b1 : qpi_mode;
