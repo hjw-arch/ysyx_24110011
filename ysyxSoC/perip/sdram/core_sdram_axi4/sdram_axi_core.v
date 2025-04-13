70381901ad3783ee5e8feeac156ef6959e169a6f
tracer-ysyx <tracer@ysyx.org>
2025-04-13 22:24:38 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:24:38 up 2 days,  4:59,  1 user,  load average: 0.47, 0.51, 0.47

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_core.v
@@ -290,7 +290,7 @@ begin
         // Another pending write request (with no refresh pending)
         if (!refresh_q && ram_req_w && (ram_wr_w != 4'b0))
         begin
-			$display("111");
+			$display("refresh = %d, ram_req_w = %d, ram_wr_w = %x", refresh_q, ram_req_w, ram_wr_w);
             // Open row hit
             if (row_open_q[addr_bank_w] && addr_row_w == active_row_q[addr_bank_w])
                 next_state_r = STATE_WRITE;
