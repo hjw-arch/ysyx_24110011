270cb5b7803a731e949ee59ddfacb5dc3bbb2be9
tracer-ysyx <tracer@ysyx.org>
2025-04-18 22:32:48 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:32:48 up 18:33,  1 user,  load average: 1.73, 0.88, 0.67

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
@@ -51,9 +51,8 @@ bool ioe_init() {
 //   __am_timer_init();
 //   __am_gpu_init();
 //   __am_audio_init();
-	if (lut[AM_UART_CONFIG] == __am_uart_config) {
-		printf("YES1\n");
-	}
+	lut[AM_UART_CONFIG] = __am_uart_config;
+	lut[AM_UART_RX] = __am_uart_rx;
   return true;
 }
 
