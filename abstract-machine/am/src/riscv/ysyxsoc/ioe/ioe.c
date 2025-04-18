c9145aba6fcefb00dd7b3c65efd4d040dcd00352
tracer-ysyx <tracer@ysyx.org>
2025-04-18 22:39:33 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:39:33 up 18:40,  1 user,  load average: 0.26, 0.57, 0.61

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
@@ -21,26 +21,25 @@ void __am_uart_rx(AM_UART_RX_T *rx);
 
 
 // static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
-// static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
-#include "stdio.h"
-static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) {printf("uart config\n"); cfg->present = true;  }
+static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
+static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
-//   [AM_TIMER_CONFIG] = __am_timer_config,
-//   [AM_TIMER_RTC   ] = __am_timer_rtc,
-//   [AM_TIMER_UPTIME] = __am_timer_uptime,
-//   [AM_INPUT_CONFIG] = __am_input_config,
-//   [AM_INPUT_KEYBRD] = __am_input_keybrd,
-//   [AM_GPU_CONFIG  ] = __am_gpu_config,
-//   [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
-//   [AM_GPU_STATUS  ] = __am_gpu_status,
-  	[AM_UART_CONFIG ] = __am_uart_config,
-	[AM_UART_RX		] = __am_uart_rx,
-//   [AM_AUDIO_CONFIG] = __am_audio_config,
-//   [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
-//   [AM_AUDIO_STATUS] = __am_audio_status,
-//   [AM_AUDIO_PLAY  ] = __am_audio_play,
+	// [AM_TIMER_CONFIG] = __am_timer_config,
+	// [AM_TIMER_RTC] = __am_timer_rtc,
+	// [AM_TIMER_UPTIME] = __am_timer_uptime,
+	[AM_INPUT_CONFIG] = __am_input_config,
+	// [AM_INPUT_KEYBRD] = __am_input_keybrd,
+	// [AM_GPU_CONFIG] = __am_gpu_config,
+	// [AM_GPU_FBDRAW] = __am_gpu_fbdraw,
+	// [AM_GPU_STATUS] = __am_gpu_status,
+	[AM_UART_CONFIG] = __am_uart_config,
+	[AM_UART_RX] = __am_uart_rx
+	// [AM_AUDIO_CONFIG] = __am_audio_config,
+	// [AM_AUDIO_CTRL] = __am_audio_ctrl,
+	// [AM_AUDIO_STATUS] = __am_audio_status,
+	// [AM_AUDIO_PLAY] = __am_audio_play,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
