834ddf0b6fde1994c5f1a62898c29aa459f0667a
tracer-ysyx <tracer@ysyx.org>
2025-04-22 22:52:54 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:52:54 up 1 day,  6:30,  1 user,  load average: 2.01, 1.20, 0.89

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/timer.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/timer.c
@@ -1,21 +1,24 @@
-// #include <am.h>
-// #include <npc.h>
-// #include <stdio.h>
-// void __am_timer_init() {
-//     outl(RTC_ADDR, 0);
-//     outl(RTC_ADDR + 4, 0);
-// }
+#include <am.h>
+#include <npc.h>
+#include <stdio.h>
 
-// void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-//     uptime->us = ((uint64_t)inl(RTC_ADDR + 4) << 32) | (uint64_t)inl(RTC_ADDR);
-// }
+#define TIMER_LOW32		*(volatile unsigned *)0x02000000
+#define TIMER_HIGH32	*(volatile unsigned *)0x02000004
 
-// void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-//     rtc->year   = inl(RTC_ADDR + 8);
-//     rtc->month  = inl(RTC_ADDR + 12);
-//     rtc->day    = inl(RTC_ADDR + 16);
-//     rtc->hour   = inl(RTC_ADDR + 20);
-//     rtc->minute = inl(RTC_ADDR + 24);
-//     rtc->second = inl(RTC_ADDR + 28);
-// }
+void __am_timer_init() {
+    return;
+}
+
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
+    uptime->us = (((uint64_t)TIMER_HIGH32 << 32) | (uint64_t)TIMER_LOW32) * 100;
+}
+
+void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
+    rtc->year   = 2025;
+    rtc->month  = 4;
+    rtc->day    = 22;
+    rtc->hour   = 13;
+    rtc->minute = 0;
+    rtc->second = 0;
+}
 
