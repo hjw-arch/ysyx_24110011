43246f3d82ae74ceb06e2ceb03dc7055a2f763ec
tracer-ysyx <tracer@ysyx.org>
2025-04-26 21:09:37 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:09:37 up 2 days,  6:13,  1 user,  load average: 0.32, 0.38, 0.40

--- ysyxSoC/perip/amba/apb_delayer.v
+++ ysyxSoC/perip/amba/apb_delayer.v
@@ -104,9 +104,9 @@ always_ff @(posedge clock) begin
 	hold_pslverr <= finish_transmit ? out_pslverr : hold_pslverr;
 end
 
-// always_ff @(posedge clock) begin
-// 	if (state != IDLE) $display("start_transmit = %d, state = %d, counter = %d, hold timer = %d, in_pready = %d, in_pwrite = %d, in_paddr = %x, hold_prdata = %x", start_transmit, state, counter, hold_timer, in_pready, in_pwrite, in_paddr, hold_prdata);
-// end
+always_ff @(posedge clock) begin
+	if (state != IDLE) $display("start_transmit = %d, state = %d, counter = %d, hold timer = %d, in_pready = %d, in_pwrite = %d, in_paddr = %x, hold_prdata = %x", start_transmit, state, counter, hold_timer, in_pready, in_pwrite, in_paddr, hold_prdata);
+end
 
 always_ff @(posedge clock) begin
 	if (in_pready)
