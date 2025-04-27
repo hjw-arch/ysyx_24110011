291b5b87b430b225f38ad81ff8efdff553f90f35
tracer-ysyx <tracer@ysyx.org>
2025-04-27 13:15:30 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:15:30 up 2 days, 15:09,  1 user,  load average: 0.23, 0.56, 0.97

--- npc/vsrc/icache.sv
+++ npc/vsrc/icache.sv
@@ -12,11 +12,23 @@
 // 	input 							clk,
 // 	input 							rst,
 
-// 	input	[ADDR_WIDTH-1:0]		addr,
-// 	input	[DATA_WIDTH-1:0]		mdata,		// 来自内存的数据
+// 	// 与IFU数据交互
+// 	input	[ADDR_WIDTH-1:0]		c2i_addr,
+// 	input							c2i_valid,
+// 	output 							i2c_ready,
 
-// 	output 	[DATA_WIDTH-1:0]		data,
-// 	output							hit			// 是否命中
+// 	output 							i2c_valid,
+// 	output 	[DATA_WIDTH-1:0]		i2c_data,
+// 	input 							c2i_ready,
+
+// 	// 与mem交互
+// 	output							i2m_valid,
+// 	output 	[ADDR_WIDTH-1:0]		i2m_addr,
+// 	input							m2i_ready,
+
+// 	input	[DATA_WIDTH-1:0]		m2i_data,
+// 	input 							m2i_valid,
+// 	output 							i2m_ready
 // );
 
 // /*********************************************** cache定义 ****************************************/
@@ -27,13 +39,29 @@
 // 	logic [DATA_WIDTH-1:0] 	data;
 // } cache_block_t;
 
-// cache_block_t cache[0:BLOCK_NUM-1];
+// cache_block_t cache [0:BLOCK_NUM-1];
 
 
 // /******************************************** 地址分解 *********************************************/
 
 // logic	[TAG_WIDTH-1:0]		tag;
+// logic	[INDEX_WIDTH-1:0]	index;
+// logic	[OFFSET_WIDTH-1:0]	offset;
 
+// assign	tag		=	c2i_addr[ADDR_WIDTH - 1 : ADDR_WIDTH - TAG_WIDTH];
+// assign	index	=	c2i_addr[INDEX_WIDTH + OFFSET_WIDTH - 1 : OFFSET_WIDTH];
+// assign	offset	=	c2i_addr[OFFSET_WIDTH - 1 : 0];
+
+
+// /***************************************** 命中检测 *************************************************/
+
+// logic 	hit;
+// assign	hit	=	cache[index].tag == tag && cache[index].valid == 1'b1;
+
+
+// /***************************************** 状态机 ***************************************************/
+
+// localparam		IDLE	=	
 
 
 	
