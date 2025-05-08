2308cc3ec253753d94cd7e1d94862455014320a7
tracer-ysyx <tracer@ysyx.org>
2025-05-08 20:12:09 +0800

    > compile NEMU ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:12:09 up 1 day, 22:39,  1 user,  load average: 0.30, 0.22, 0.18

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -155,7 +155,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-	if (IN_UART(addr)) printf("PADDR_WRITE\n");
+	if (IN_UART(addr)) printf("PADDR_WRITE, %x\n", addr);
     if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
     IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
 	if(IN_UART(addr)) IFDEF(CONFIG_SOC, sim_uart_write(addr, (uint8_t)data); return);
