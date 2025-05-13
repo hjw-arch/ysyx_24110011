454fe9ab51c1c4ee9b9101e3173bbe0d615f5291
tracer-ysyx <tracer@ysyx.org>
2025-05-13 16:07:45 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:07:45 up 4 days, 19:24,  1 user,  load average: 0.49, 0.67, 0.51

--- ysyxSoC/perip/amba/axi4_delayer.v
+++ ysyxSoC/perip/amba/axi4_delayer.v
@@ -63,305 +63,6 @@ module axi4_delayer(
   input  [1:0]  out_bresp
 );
 
-
-// ------------------------------------------------------------
-// -- Calibrating memory access latency (c = r * s * k)
-// ------------------------------------------------------------
-// yosys-sta core timing report: 507.418MHz
-//    with device clk frequency: 100MHz
-// 
-// got r = core_clk_freq / device_clk_freq = 5,
-// and let's assume s = 2
-// ------------------------------------------------------------
-
-  localparam r = 6;
-  localparam s = 16;
-  localparam inc = 99;
-
-  localparam S_IDLE = 3'd0, S_TRANS = 3'd1, S_WAIT = 3'd2;
-  localparam S_BURST_0 = 3'd3, S_BURST_1 = 3'd4, S_BURST_2 = 3'd5, S_BURST_3 = 3'd6; 
-  reg [ 2: 0] r_state;
-  reg [ 2: 0] burst_state;
-  reg [ 2: 0] w_state;
-
-  reg [31: 0] r_counters;
-  reg [31: 0] r_quant_counters;
-  reg [31: 0] r_cnt_burst_0;
-  reg [31: 0] r_cnt_burst_1;
-  reg [31: 0] r_cnt_burst_2;
-  reg [31: 0] r_cnt_burst_3;
-
-  reg [31: 0] w_counters;
-  reg [31: 0] w_quant_counters;
-
-  reg         rvalid_beat_0_q;
-  reg [ 3: 0] rid_beat_0_q;
-  reg [31: 0] rdata_beat_0_q;
-  reg [ 1: 0] rresp_beat_0_q;
-  reg         rlast_beat_0_q;
-
-  reg         rvalid_beat_1_q;
-  reg [ 3: 0] rid_beat_1_q;
-  reg [31: 0] rdata_beat_1_q;
-  reg [ 1: 0] rresp_beat_1_q;
-  reg         rlast_beat_1_q;
-
-  reg         rvalid_beat_2_q;
-  reg [ 3: 0] rid_beat_2_q;
-  reg [31: 0] rdata_beat_2_q;
-  reg [ 1: 0] rresp_beat_2_q;
-  reg         rlast_beat_2_q;
-
-  reg         rvalid_beat_3_q;
-  reg [ 3: 0] rid_beat_3_q;
-  reg [31: 0] rdata_beat_3_q;
-  reg [ 1: 0] rresp_beat_3_q;
-  reg         rlast_beat_3_q;
-
-  reg         bvalid_q;
-  reg [ 3: 0] bid_q;
-  reg [ 1: 0] bresp_q;
-
-
-  wire        r_hs = out_rvalid & out_rready;
-  wire        b_hs = out_bvalid & out_bready;
-
-// read fsm //////////////////////////////////////////////////////////////////////////////////////
-
-  always @ (posedge clock) begin
-    if (reset)
-      r_state <= S_IDLE;
-    else begin
-      case (r_state)
-        S_IDLE    : r_state <= in_arvalid            ? 
-                    (in_arlen == 8'd3                ? S_BURST_0 : S_BURST_3) : S_IDLE;
-        S_BURST_0 : r_state <= r_hs                  ? S_BURST_1 : S_BURST_0;
-        S_BURST_1 : r_state <= r_hs                  ? S_BURST_2 : S_BURST_1;
-        S_BURST_2 : r_state <= r_hs                  ? S_BURST_3 : S_BURST_2;
-        S_BURST_3 : r_state <= r_hs & out_rlast      ? S_WAIT    : S_BURST_3;        
-        S_WAIT    : r_state <= r_cnt_burst_3 == 0    ? (in_arvalid ? S_BURST_0 : S_IDLE) : S_WAIT;
-        default   : r_state <= r_state;
-      endcase
-    end 
-  end
-
-  always @ (posedge clock) begin
-    if (reset)
-      burst_state <= S_IDLE;
-    else begin
-      case (burst_state)
-        S_IDLE    : burst_state <= r_hs               ? 
-                    (in_arlen == 8'd3                 ? S_BURST_0 : S_BURST_3) : S_IDLE;
-        S_BURST_0 : burst_state <= r_cnt_burst_0 == 0 ? S_BURST_1 : S_BURST_0;
-        S_BURST_1 : burst_state <= r_cnt_burst_1 == 0 ? S_BURST_2 : S_BURST_1;
-        S_BURST_2 : burst_state <= r_cnt_burst_2 == 0 ? S_BURST_3 : S_BURST_2;
-        S_BURST_3 : burst_state <= r_cnt_burst_3 == 0 ? S_IDLE    : S_BURST_3;
-        default   : burst_state <= S_IDLE;   
-      endcase
-    end
-  end
-
-// read quant counters //////////////////////////////////////////////////////////////////////////
-
-  wire r_transfer = r_state == S_BURST_0 | r_state == S_BURST_1 | 
-                  r_state == S_BURST_2 | r_state == S_BURST_3 ;
-  wire r_waiting  = r_state == S_WAIT;
-
-  always @ (posedge clock) begin
-    if (reset)
-      r_quant_counters <= 32'd0;
-    else if (r_transfer)
-      r_quant_counters <= r_quant_counters + inc;
-    else if (r_waiting)
-      r_quant_counters <= 32'd0;
-  end
-
-  always @ (posedge clock) begin
-    if (reset)
-      r_counters <= 32'd0;
-    else if (r_transfer)
-      r_counters <= r_counters + 1;
-    else if (r_counters == 0)
-      r_counters <= 32'd0;
-    else if (r_waiting)
-      r_counters <= 32'd0;
-  end
-
-// read burst counters //////////////////////////////////////////////////////////////////////////
-
-  wire idle    = (r_state == S_IDLE   );
-  wire burst_0 = (r_state == S_BURST_0);
-  wire burst_1 = (r_state == S_BURST_1);
-  wire burst_2 = (r_state == S_BURST_2);
-  wire burst_3 = (r_state == S_BURST_3);
-
-  always @ (posedge clock) begin
-    if (reset)
-      r_cnt_burst_0 <= 32'd0;
-    else if (burst_0 & r_hs)
-      r_cnt_burst_0 <= ((r_quant_counters + inc) >> $clog2(s)) - r_counters -2;
-    else if (r_cnt_burst_0 == 0)
-      r_cnt_burst_0 <= 32'd0;
-    else 
-      r_cnt_burst_0 <= r_cnt_burst_0 - 1;
-  end
-
-  always @ (posedge clock) begin
-    if (reset)
-      r_cnt_burst_1 <= 32'd0;
-    else if (burst_1 & r_hs)
-      r_cnt_burst_1 <= ((r_quant_counters + inc) >> $clog2(s)) - r_counters - 2;
-    else if (r_cnt_burst_1 == 0)
-      r_cnt_burst_1 <= 32'd0;
-    else 
-      r_cnt_burst_1 <= r_cnt_burst_1 - 1;
-  end
-
-  always @ (posedge clock) begin
-    if (reset)
-      r_cnt_burst_2 <= 32'd0;
-    else if (burst_2 & r_hs)
-      r_cnt_burst_2 <= ((r_quant_counters + inc) >> $clog2(s)) - r_counters - 2;
-    else if (r_cnt_burst_2 == 0)
-      r_cnt_burst_2 <= 32'd0;
-    else 
-      r_cnt_burst_2 <= r_cnt_burst_2 - 1;
-  end
-
-  always @ (posedge clock) begin
-    if (reset)
-      r_cnt_burst_3 <= 32'd0;
-    else if (burst_3 & r_hs & out_rlast)
-      r_cnt_burst_3 <= ((r_quant_counters + inc) >> $clog2(s)) - r_counters - 2;
-    else if (r_cnt_burst_3 == 0)
-      r_cnt_burst_3 <= 32'd0;
-    else 
-      r_cnt_burst_3 <= r_cnt_burst_3 - 1;
-  end
-
-// read burst registers //////////////////////////////////////////////////////////////////////////
-  
-  always @ (posedge clock) begin
-    if (reset) begin
-      rvalid_beat_0_q <= 0;   
-      rid_beat_0_q    <= 0;   
-      rdata_beat_0_q  <= 0; 
-      rresp_beat_0_q  <= 0; 
-      rlast_beat_0_q  <= 0; 
-    end else if (r_hs & burst_0) begin
-      rvalid_beat_0_q <= out_rvalid;  
-      rid_beat_0_q    <= out_rid;
-      rdata_beat_0_q  <= out_rdata;
-      rresp_beat_0_q  <= out_rresp;
-      rlast_beat_0_q  <= out_rlast;
-    end
-  end
-
-  always @ (posedge clock) begin
-    if (reset) begin
-      rvalid_beat_1_q <= 0;   
-      rid_beat_1_q    <= 0;   
-      rdata_beat_1_q  <= 0; 
-      rresp_beat_1_q  <= 0; 
-      rlast_beat_1_q  <= 0; 
-    end else if (r_hs & burst_1) begin 
-      rvalid_beat_1_q <= out_rvalid;  
-      rid_beat_1_q    <= out_rid;
-      rdata_beat_1_q  <= out_rdata;
-      rresp_beat_1_q  <= out_rresp;
-      rlast_beat_1_q  <= out_rlast;
-    end
-  end
-
-  always @ (posedge clock) begin
-    if (reset) begin
-      rvalid_beat_2_q <= 0;   
-      rid_beat_2_q    <= 0;   
-      rdata_beat_2_q  <= 0; 
-      rresp_beat_2_q  <= 0; 
-      rlast_beat_2_q  <= 0; 
-    end else if (r_hs & burst_2) begin 
-      rvalid_beat_2_q <= out_rvalid;  
-      rid_beat_2_q    <= out_rid;
-      rdata_beat_2_q  <= out_rdata;
-      rresp_beat_2_q  <= out_rresp;
-      rlast_beat_2_q  <= out_rlast;
-    end
-  end
-
-  always @ (posedge clock) begin
-    if (reset) begin
-      rvalid_beat_3_q <= 0;   
-      rid_beat_3_q    <= 0;   
-      rdata_beat_3_q  <= 0; 
-      rresp_beat_3_q  <= 0; 
-      rlast_beat_3_q  <= 0; 
-    end else if (r_hs & out_rlast & burst_3) begin 
-      rvalid_beat_3_q <= out_rvalid;  
-      rid_beat_3_q    <= out_rid;
-      rdata_beat_3_q  <= out_rdata;
-      rresp_beat_3_q  <= out_rresp;
-      rlast_beat_3_q  <= out_rlast;
-    end
-  end
-
-// write fsm /////////////////////////////////////////////////////////////////////////////////////
-
-  always @ (posedge clock) begin
-    if (reset)
-      w_state <= S_IDLE;
-    else begin
-      case (w_state)
-        S_IDLE  : w_state <= in_awvalid            ? S_TRANS : S_IDLE;
-        S_TRANS : w_state <= b_hs                  ? S_WAIT  : S_TRANS;
-        S_WAIT  : w_state <= w_quant_counters == 0 ? (in_awvalid ? S_TRANS : S_IDLE) : S_WAIT;
-        default : w_state <= w_state;
-      endcase
-    end
-  end
-
-  // write quant counters //////////////////////////////////////////////////////////////////////////
-
-  wire w_transfer = w_state == S_TRANS;
-  wire w_transfer2waiting = b_hs & w_transfer;
-  wire w_waiting  = w_state == S_WAIT;
-
-  always @ (posedge clock) begin
-    if (reset)
-      w_quant_counters <= 32'd0;
-    else if (w_transfer2waiting)
-      w_quant_counters <= ((w_quant_counters + inc) >> $clog2(s)) - 1;
-    else if (w_transfer)
-      w_quant_counters <= w_quant_counters + inc;
-    else if (w_quant_counters == 0)
-      w_quant_counters <= 32'd0;
-    else if (w_waiting)
-      w_quant_counters <= w_quant_counters - 1;
-  end
-
-  always @ (posedge clock) begin
-    if (reset)
-      w_counters <= 32'd0;
-    else if (w_transfer)
-      w_counters <= w_counters + 1;
-    else if (w_counters == 0)
-      w_counters <= 32'd0;
-    else if (w_waiting)
-      w_counters <= 32'd0;
-  end
-
-  always @ (posedge clock) begin
-    if (reset) begin
-      bvalid_q <= 0;
-      bid_q    <= 0;
-      bresp_q  <= 0;
-    end else if (w_transfer & b_hs) begin
-      bvalid_q <= out_bvalid;
-      bid_q    <= out_bid;
-      bresp_q  <= out_bresp;
-    end
-  end
-
   assign in_arready = out_arready;
   assign out_arvalid = in_arvalid;
   assign out_arid = in_arid;
@@ -369,27 +70,10 @@ module axi4_delayer(
   assign out_arlen = in_arlen;
   assign out_arsize = in_arsize;
   assign out_arburst = in_arburst;
-  assign out_rready = in_rready;
-  assign in_rvalid = burst_state == S_BURST_3 & r_cnt_burst_3 == 0 ? rvalid_beat_3_q :
-                     burst_state == S_BURST_2 & r_cnt_burst_2 == 0 ? rvalid_beat_2_q :
-                     burst_state == S_BURST_1 & r_cnt_burst_1 == 0 ? rvalid_beat_1_q :
-                     burst_state == S_BURST_0 & r_cnt_burst_0 == 0 ? rvalid_beat_0_q : 0;
-  assign in_rid = burst_state == S_BURST_3 & r_cnt_burst_3 == 0 ? rid_beat_3_q :
-                  burst_state == S_BURST_2 & r_cnt_burst_2 == 0 ? rid_beat_2_q :
-                  burst_state == S_BURST_1 & r_cnt_burst_1 == 0 ? rid_beat_1_q :
-                  burst_state == S_BURST_0 & r_cnt_burst_0 == 0 ? rid_beat_0_q : 0;
-  assign in_rdata = burst_state == S_BURST_3 & r_cnt_burst_3 == 0 ? rdata_beat_3_q :
-                    burst_state == S_BURST_2 & r_cnt_burst_2 == 0 ? rdata_beat_2_q :
-                    burst_state == S_BURST_1 & r_cnt_burst_1 == 0 ? rdata_beat_1_q :
-                    burst_state == S_BURST_0 & r_cnt_burst_0 == 0 ? rdata_beat_0_q : 0;
-  assign in_rresp = burst_state == S_BURST_3 & r_cnt_burst_3 == 0 ? rresp_beat_3_q :
-                    burst_state == S_BURST_2 & r_cnt_burst_2 == 0 ? rresp_beat_2_q :
-                    burst_state == S_BURST_1 & r_cnt_burst_1 == 0 ? rresp_beat_1_q :
-                    burst_state == S_BURST_0 & r_cnt_burst_0 == 0 ? rresp_beat_0_q : 0;
-  assign in_rlast = burst_state == S_BURST_3 & r_cnt_burst_3 == 0 ? rlast_beat_3_q :
-                    burst_state == S_BURST_2 & r_cnt_burst_2 == 0 ? rlast_beat_2_q :
-                    burst_state == S_BURST_1 & r_cnt_burst_1 == 0 ? rlast_beat_1_q :
-                    burst_state == S_BURST_0 & r_cnt_burst_0 == 0 ? rlast_beat_0_q : 0;
+  assign in_rid = out_rid;
+  assign in_rdata = out_rdata;
+  assign in_rresp = out_rresp;
+  assign in_rlast = out_rlast;
   assign in_awready = out_awready;
   assign out_awvalid = in_awvalid;
   assign out_awid = in_awid;
@@ -402,9 +86,226 @@ module axi4_delayer(
   assign out_wdata = in_wdata;
   assign out_wstrb = in_wstrb;
   assign out_wlast = in_wlast;
-  assign out_bready = in_bready;
-  assign in_bvalid = (w_quant_counters == 0 & w_waiting) ? bvalid_q : 0;
-  assign in_bid = (w_quant_counters == 0 & w_waiting) ? bid_q : 0;
-  assign in_bresp = (w_quant_counters == 0 & w_waiting) ? bresp_q : 0;
+  assign in_bid = out_bid;
+  assign in_bresp = out_bresp;
+
+
+
+/*****************************************************************************/
+// 常量定义
+localparam SR        =        99;     // 6.2*16
+localparam SHIFTER   =        4;
+
+localparam  IDLE     = 2'b00,
+            RUNNING  = 2'b01,
+            DELAYING = 2'b11;
+
+// 信号定义            
+/*****************************************************************************/
+// 读通道延迟逻辑 (AR & R channels)
+/*****************************************************************************/
+
+// 读通道状态和计数器
+logic [1:0] r_state, r_state_last, n_r_state;
+logic arvalid_last;
+logic [7:0] r_counter;           // 读请求计数器
+logic [19:0] r_delay_counter;    // 读累积延迟计数器
+
+// 读通道状态转换检测
+logic read_state_changing_to_delay;
+
+// 握手成功标志
+logic new_handshake;
+
+// 边沿检测
+always_ff @(posedge clock) begin
+    arvalid_last <= in_arvalid;
+    r_state_last <= r_state;
+	new_handshake <= (in_rvalid & out_rready & ~in_rlast);
+end
+
+wire read_start = ~arvalid_last & in_arvalid;
+
+// 读通道状态转换检测
+assign read_state_changing_to_delay = (r_state_last == RUNNING) && (r_state == DELAYING);
+
+// 读通道状态机逻辑
+always_ff @(posedge clock) begin
+    if (reset) begin
+        r_state <= IDLE;
+    end else begin
+        r_state <= n_r_state;
+    end
+end
+
+// 读通道下一状态逻辑
+always_comb begin
+    n_r_state = r_state;
+
+    case (r_state)
+        IDLE: begin
+            if (read_start | (in_rvalid & out_rready & ~in_rlast)) n_r_state = RUNNING;
+        end
+        RUNNING: begin
+            if (out_rvalid) n_r_state = DELAYING;
+        end
+        DELAYING: begin // 0时要握手成功，1时就得交互，再补偿开始时少减的1，就是2
+            if (r_delay_counter == 2) n_r_state = IDLE;
+        end
+        default: n_r_state = IDLE;
+    endcase
+end
+
+// 读通道计数器和延迟计算逻辑
+always_ff @(posedge clock) begin
+    if (reset) begin
+        r_counter <= 0;
+        r_delay_counter <= 0;
+    end else begin
+        if (read_start | new_handshake) begin
+            // 读请求开始
+            r_counter <= 1;
+            r_delay_counter <= SR;
+        end else if (r_state == RUNNING) begin		// 最后一次的话，无需计入，因为CPU直接成功了
+            // 累积延迟
+            r_counter <= r_counter + 1;
+            if(~out_rlast) r_delay_counter <= r_delay_counter + SR;
+        end
+        
+        // 状态转换时计算延迟周期数
+        if (read_state_changing_to_delay) begin
+            r_delay_counter <= (r_delay_counter >> SHIFTER) - {12'b0, r_counter};
+        end else if (r_state == DELAYING && r_delay_counter > 0) begin
+            // 在DELAYING状态下简单递减
+            r_delay_counter <= r_delay_counter - 1;
+        end
+    end
+end
+
+// 延迟读控制逻辑
+logic delay_rok;
+
+// 延迟读控制（现在使用r_remaining_delay来决定何时结束延迟）
+always_ff @(posedge clock) begin
+    if (reset) begin
+        delay_rok <= 1'b0;
+    end else begin
+        if (r_state == DELAYING && r_delay_counter == 2) begin
+            // 当延迟即将结束时，开始传输有效信号
+            delay_rok <= 1'b1;
+        end else if (in_rvalid && out_rready) begin
+            // 传输完成后清除
+            delay_rok <= 1'b0;
+        end
+    end
+end
+
+// 读通道代理逻辑
+assign out_rready = delay_rok & in_rready;
+assign in_rvalid = delay_rok & out_rvalid;
+
+
+
+/*****************************************************************************/
+// 写通道延迟逻辑 (AW, W & B channels)
+/*****************************************************************************/
+
+// 写通道状态和计数器
+logic [1:0] w_state, w_state_last, n_w_state;
+logic awvalid_last, wvalid_last;
+logic [7:0] w_counter;
+logic [19:0] w_delay_counter;
+
+// 写通道状态转换检测
+logic write_state_changing_to_delay;
+
+// 写通道边沿检测
+always_ff @(posedge clock) begin
+    awvalid_last <= in_awvalid;
+    wvalid_last <= in_wvalid;
+    w_state_last <= w_state;
+end
+
+wire write_start = ~awvalid_last & in_awvalid | ~wvalid_last & in_wvalid;
+
+// 写通道状态转换检测
+assign write_state_changing_to_delay = (w_state_last == RUNNING) && (w_state == DELAYING);
+
+// 写通道状态机逻辑
+always_ff @(posedge clock) begin
+    if (reset) begin
+        w_state <= IDLE;
+    end else begin
+        w_state <= n_w_state;
+    end
+end
+
+// 写通道下一状态逻辑
+always_comb begin
+    n_w_state = w_state;
+    
+    case (w_state)
+        IDLE: begin
+            if (write_start) n_w_state = RUNNING;
+        end
+        RUNNING: begin
+            if (out_bvalid) n_w_state = DELAYING; // 写数据完成时进入延迟状态
+        end
+        DELAYING: begin
+            if (w_delay_counter == 2) n_w_state = IDLE;
+        end
+        default: n_w_state = IDLE;
+    endcase
+end
+
+// 写通道计数器和延迟计算逻辑
+always_ff @(posedge clock) begin
+    if (reset) begin
+        w_counter <= 0;
+        w_delay_counter <= 0;
+    end else begin
+        if (write_start) begin
+            // 写请求开始
+            w_counter <= 1;
+            w_delay_counter <= SR;
+        end else if (w_state == RUNNING) begin
+            // 累积延迟
+            w_counter <= w_counter + 1;
+            if (~out_bvalid) w_delay_counter <= w_delay_counter + SR;
+        end
+        
+        // 状态转换时计算延迟周期数
+        if (write_state_changing_to_delay) begin
+            w_delay_counter <= (w_delay_counter >> SHIFTER) - {12'b0, w_counter};
+        end else if (w_state == DELAYING && w_delay_counter > 0) begin
+            // 在DELAYING状态下简单递减
+            w_delay_counter <= w_delay_counter - 1;
+        end
+    end
+end
+
+// 延迟写控制逻辑
+logic delay_wok;
+
+// 写响应通道控制
+always_ff @(posedge clock) begin
+    if (reset) begin
+        delay_wok <= 1'b0;
+    end else begin
+        if (w_state == DELAYING && w_delay_counter == 2) begin
+            // 当延迟即将结束时，传递B通道响应
+            delay_wok <= 1'b1;
+        end else if (in_bvalid && out_bready) begin
+            // 响应被接收后清除
+            delay_wok <= 1'b0;
+        end
+    end
+end
+
+// 写通道代理连接
+
+assign in_bvalid = delay_wok & out_bvalid;
+assign out_bready = delay_wok & in_bready;
 
 endmodule
+
