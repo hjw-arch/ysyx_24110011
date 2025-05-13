fed59cc74f4e941d2cbc675b119cb21f92a0c062
tracer-ysyx <tracer@ysyx.org>
2025-05-13 16:43:16 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:43:16 up 4 days, 20:00,  1 user,  load average: 0.67, 0.34, 0.31

--- ysyxSoC/perip/amba/axi4_delayer.v
+++ ysyxSoC/perip/amba/axi4_delayer.v
@@ -169,7 +169,7 @@ always_ff @(posedge clock) begin
         end else if (r_state == RUNNING) begin		// 最后一次的话，无需计入，因为CPU直接成功了
             // 累积延迟
             r_counter <= r_counter + 1;
-            r_delay_counter <= (out_rlast) ? r_delay_counter : r_delay_counter + SR;
+            r_delay_counter <= (out_rvalid & out_rlast) ? r_delay_counter : r_delay_counter + SR;
         end
         
         // 状态转换时计算延迟周期数
