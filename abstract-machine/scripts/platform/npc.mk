5c950ee09f8abb4dc1a924393f163d64156f8fd6
tracer-ysyx <tracer@ysyx.org>
2026-06-01 17:04:29 +0800

    > sim RTL ysyx_24110011 胡晋玮 Linux ysyx 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:04:29 up 4 days, 19:12,  1 user,  load average: 1.19, 0.53, 0.45

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -18,7 +18,9 @@ CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/riscv/npc/ioe -I$(AM_HOME)/am/src/riscv/
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
-NPCARGS = -e $(IMAGE).elf -d ./libnemu.so
+NPC_ISA = $(if $(findstring riscv32e,$(ARCH)),RVE,RVI)
+
+NPCARGS = -e $(IMAGE).elf -d ./libnemu.so -b
 
 image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
@@ -26,4 +28,4 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NPC_HOME) NPCARGS="$(NPCARGS)" npc IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) NPC_ISA=$(NPC_ISA) NPCARGS="$(NPCARGS)" npc IMG=$(IMAGE).bin
